799c5b24de2302632c0ca108c2e755d66275c9a1
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-10-02 11:35:36 -0300

    WL Project Trybewallet 1 e 2/6 vX4 LINT X.X

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
-import { useSelector } from "react-redux";
-import { GlobalState } from "../types";
+import { useSelector } from 'react-redux';
+import { GlobalState } from '../types';
 
 function Header() {
   const email = useSelector((state: GlobalState) => state.user.email);
   console.log(email);
   return (
     <header>
-      <p data-testid="email-field">Login: {email}</p>
+      <p data-testid="email-field">
+        Login: 
+        {email}
+      </p>
       <p data-testid="total-field">Total: 0</p>
       <p data-testid="header-currency-field">BRL</p>
     </header>
