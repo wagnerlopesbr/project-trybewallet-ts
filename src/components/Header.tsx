79ff390b374cd72ae16619269b5b59934bbf4de0
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-10-02 09:35:11 -0300

    WL - Project Trybewallet 2/6 v2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,18 @@
+import { useSelector } from "react-redux";
+import { GlobalState } from "../types";
+
 function Header() {
-  return <div>Header</div>;
-  // first commit
+  const loginState = useSelector((state: GlobalState) => state.login);
+  return (
+    <header>
+      <p data-testid="email-field">
+        Login:
+        {loginState.email}
+      </p>
+      <p data-testid="total-field">Total: 0</p>
+      <p data-testid="header-currency-field">BRL</p>
+    </header>
+  );
 }
 
 export default Header;
--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,17 +1,9 @@
-import { useSelector } from "react-redux";
-import { GlobalState } from "../types";
+import Header from '../components/Header';
 
 function Wallet() {
-  const loginState = useSelector((state: GlobalState) => state.login);
   return (
     <div>
-      <header>Wallet</header>
-      <p data-testid="email-field">
-        Login:
-        {loginState.email}
-      </p>
-      <p data-testid="total-field">Total: 0</p>
-      <p data-testid="header-currency-field">BRL</p>
+      <Header />
     </div>
   );
 }
